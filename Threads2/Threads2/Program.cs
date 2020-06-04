9d2c928f6a9fd2a2b4ce9c855c25d867b21f93f0
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-06-04 12:21:52 -0700

    thread pools

--- Threads2/Threads2/Program.cs
+++ Threads2/Threads2/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.ComponentModel.Design;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
+
 namespace Threads2
 {
     class Program
@@ -15,28 +18,42 @@ namespace Threads2
             //Thread.Sleep(1000);
             //Console.WriteLine("Hello World! 4");
 
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 1");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 2");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 3");
-            }).Start();
-            new Thread(() =>
-            {
-                Thread.Sleep(1000);
-                Console.WriteLine("Thread 4");
-            }).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 1");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 2");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 3");
+            //}).Start();
+            //new Thread(() =>
+            //{
+            //    Thread.Sleep(1000);
+            //    Console.WriteLine("Thread 4");
+            //}).Start();
 
-            Console.ReadKey();
+
+            Enumerable.Range(0, 1000).ToList().ForEach(f =>
+             {
+                 ThreadPool.QueueUserWorkItem((o) =>
+                 {
+                     Console.WriteLine($"Thread number:{Thread.CurrentThread.ManagedThreadId} started");
+                     Thread.Sleep(1000);
+
+                     Console.WriteLine($"Thread number:{Thread.CurrentThread.ManagedThreadId} ended");
+                 });
+             });       
+           
+            Console.ReadLine();
+
+          
         }
     }
 }
