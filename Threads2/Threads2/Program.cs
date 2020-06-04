c98912b91aa8d3d0172ff46fe125fa861b2b2e96
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-06-04 11:40:40 -0700

    Thread basics

---
+++ Threads2/Threads2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Threading;
+namespace Threads2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World! 1");
+            //Thread.Sleep(1000); //Stops the main thread of a WPF UI
+            //Console.WriteLine("Hello World! 2");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 3");
+            //Thread.Sleep(1000);
+            //Console.WriteLine("Hello World! 4");
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
